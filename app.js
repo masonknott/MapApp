7a31a857b969cd3bf0ed39b9b96e611f9a6dbc49
masonknott <42193604+masonknott@users.noreply.github.com>
2024-02-21 14:59:36 +0700

    map app

---
+++ app.js
@@ -0,0 +1,62 @@
+  var map = L.map('map').setView([7.8804, 98.3923], 13);
+
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                 '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                 'Imagery © <a href="https://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11', // Or another Mapbox style that suits your needs
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoidG9tdGs5OSIsImEiOiJjbHNnMzRkYzQxaHNjMmtvMXRrMHZzZjRpIn0.P636xQ6a2gWcw-PsVrmQFg', // Replace with your Mapbox access token
+    language: 'en' // This parameter sets the language of the map labels to English
+}).addTo(map);
+
+  // Initialize the geocoder
+  var geocoder = L.Control.Geocoder.nominatim();
+
+  function flyToContinent(continent) {
+    var coordinates = {
+      'Africa': [-8.7832, 34.5085],
+      'Asia': [34.0479, 100.6197],
+      'Europe': [54.5260, 15.2551],
+      'North America': [54.5260, -105.2551],
+      'South America': [-8.7832, -55.4915],
+      'Australia': [-25.2744, 133.7751],
+    };
+    map.flyTo(coordinates[continent], 3);
+  }
+
+  function geocodeSearch() {
+    var input = document.getElementById('search').value;
+    if (input && input.length > 0) {
+      geocoder.geocode(input, function (results) {
+        if (results.length > 0) {
+          var r = results[0];
+          if (r.bbox && r.bbox.length === 4) {
+                // Fit the map bounds to the geocode result, then set a zoom level
+            var bounds = [
+              [r.bbox[0], r.bbox[2]],
+              [r.bbox[1], r.bbox[3]]
+            ];
+            map.fitBounds(bounds).once('zoomend', function() {
+              // Set a specific zoom level after fitting bounds, if necessary
+              // Adjust the zoom level (e.g., to 15) as per your requirement
+              map.setZoom(15);
+            });
+          } else if (r.center && r.center.lat !== undefined && r.center.lng !== undefined) {
+            // Pan to the geocode result and set a specific zoom level
+            map.setView(new L.LatLng(r.center.lat, r.center.lng), 15); // Set zoom level to 15
+          } else {  
+            alert('Geocoding successful but no bounding box found.');
+          }
+        } else {
+          alert('No results found');
+        }
+      });
+    } else {
+      alert('Please enter a search term');
+    }
+}
+    // Optional: Automatically focus on the search input when the page loads
